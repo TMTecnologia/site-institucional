7d14ce12012f70be7ea997f58a1d67a08fd9050d
Thales Menezes <thlmenezes@pm.me>
2022-10-03 20:48:50 -0300

    💡 todo note

--- headless-blog/src/api/post/content-types/post/lifecycles.ts
+++ headless-blog/src/api/post/content-types/post/lifecycles.ts
@@ -5,6 +5,8 @@ export default {
     const { data } = params;
     if (data?.content && data.content?.length > 0) {
       data.readingTime = readingTime(data.content).text;
+      // TODO: instead of english generated text of time,
+      // we could just save the minutes as int
     }
   },
   async beforeUpdate({params}) {
